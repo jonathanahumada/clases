942ed3b733b4cab06c295a3c90d7c5d97e0ba626
jaumaf <jaumaf@hotmail.com>
2019-04-08 17:24:35 -0500

    actualizacion abril 8
    Former-commit-id: 570a3068f5ddfe05460f4a26890ee1445bcd47bf

---
+++ clt/compilar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+ cat bitacora.html > ~/.devs/jonathanahumada.github.io/index.html 
+ cat lectura_1.html > ~/.devs/jonathanahumada.github.io/lectura_1.html 
+ cat lectura_2.html > ~/.devs/jonathanahumada.github.io/lectura_2.html 
+ cat lectura3.html > ~/.devs/jonathanahumada.github.io/lectura3.html 
+ cat plazas.html > ~/.devs/jonathanahumada.github.io/plazas.html 
+ cat plazas_yerbateras.html > ~/.devs/jonathanahumada.github.io/plazas_yerbateras.html 
